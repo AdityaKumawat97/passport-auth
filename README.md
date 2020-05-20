40e8d6eb56e04f69d4535e0db13e45d6db97bb74
Aditya_kumawat <31047132+AdityaKumawat97@users.noreply.github.com>
2020-05-20 22:24:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Passport-Authentication
+ Using passport js. for authentication in a react app
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            Passport Starter!
+        </div>
+    );
+};
+
+export default App;
